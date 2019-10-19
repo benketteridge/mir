1a1c19ab1e518f99b1a3e263ab1efa85a602af45
Ben Ketteridge <ben.ketteridge@gmail.com>
2019-10-19 15:06:28 +0100

    add transaction handling to the spend API
    to implement optimistic locking

--- src/main/java/com/ketteridge/mir/handlers/SpendHandler.java
+++ src/main/java/com/ketteridge/mir/handlers/SpendHandler.java
@@ -16,6 +16,8 @@ import static ratpack.jackson.Jackson.fromJson;
 @Slf4j
 public class SpendHandler extends ExtendedHandler {
 
+    private static final int MAX_TRIES = 5;
+
     @Override
     public boolean supports(Context ctx) {
         return ctx.getRequest().getMethod().isPost() && ctx.getRequest().getPath().equals("spend");
@@ -27,24 +29,49 @@ public class SpendHandler extends ExtendedHandler {
         // we must be authenticated to have got this far:
         ctx.parse(fromJson(Transaction.class)).then(transaction -> {
             try (Jedis jedis = getPool(ctx).getResource()) {
-                // return the transactions against the hash
-                String key1 = "transactions:" + getAuth(ctx);
-                String key2 = "balance:" + getAuth(ctx);
-                String currentTransactions = jedis.get(key1);
-                List<Transaction> transactions = mapper.readValue(currentTransactions, new TypeReference<List<Transaction>>(){});
+                int failed = 0;
+                do {
 
-                // hmm what do we do about mis-matched currencies? (error, or handle exchange rates, or separate balances?)
-                // or outspending the balance?
-                transactions.add(transaction.spend());
+                    // return the transactions against the hash
+                    String key1 = "transactions:" + getAuth(ctx);
+                    String key2 = "balance:" + getAuth(ctx);
 
-                BigDecimal summation = transactions.stream().map(Transaction::getAmountBD).reduce(BigDecimal.ZERO, BigDecimal::add);
+                    // implementing optimistic locking, with five tries, and 1 second back-off
+                    jedis.watch(key1, key2);
 
-                String newTransactionsList = mapper.writeValueAsString(transactions);
-                jedis.set(key1, newTransactionsList);
-                jedis.set(key2, transactions.get(0).getCurrency() + " " + summation.toPlainString());
+                    String currentTransactions = jedis.get(key1);
+                    List<Transaction> transactions = mapper.readValue(currentTransactions, new TypeReference<List<Transaction>>() {
+                    });
+
+                    // hmm what do we do about mis-matched currencies? (error, or handle exchange rates, or separate balances?)
+                    // or outspending the balance?
+                    transactions.add(transaction.spend());
+
+                    BigDecimal summation = transactions.stream().map(Transaction::getAmountBD).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+                    String newTransactionsList = mapper.writeValueAsString(transactions);
+
+                    redis.clients.jedis.Transaction multi = jedis.multi();
+
+                    multi.set(key1, newTransactionsList);
+                    multi.set(key2, transactions.get(0).getCurrency() + " " + summation.toPlainString());
+
+                    if (multi.exec().isEmpty()) {
+                        failed++;
+                        // don't hammer the server
+                        Thread.sleep(1000);
+                    }
+                    else failed = -1;
+                }
+                while (failed > 0 && failed < MAX_TRIES);
+
+                if (failed < 0) {
+                    ctx.getResponse().status(201).send("ok");
+                }
+                else {
+                    ctx.getResponse().status(500).send("unable to complete request");
+                }
             }
-
-            ctx.getResponse().status(201).send("ok");
         });
     }
 }
