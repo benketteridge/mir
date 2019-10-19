ae3ca419df550201a0c0e823b38bfab059503051
Ben Ketteridge <ben.ketteridge@gmail.com>
2019-10-19 15:22:29 +0100

    testing works with mock Jedis

--- src/test/java/com/ketteridge/mir/ApplicationTest.java
+++ src/test/java/com/ketteridge/mir/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.ketteridge.mir;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,17 +10,23 @@ import ratpack.impose.ImpositionsSpec;
 import ratpack.impose.UserRegistryImposition;
 import ratpack.registry.Registry;
 import ratpack.test.MainClassApplicationUnderTest;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Transaction;
+
+import java.util.Collections;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class ApplicationTest {
 
 //    private JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
     private JedisPool pool = mock(JedisPool.class);
+    private Jedis jedis = mock(Jedis.class);
 
     private MainClassApplicationUnderTest appUnderTest = new MainClassApplicationUnderTest(Application.class) {
         @Override
@@ -27,6 +34,11 @@ public class ApplicationTest {
             impositions.add(UserRegistryImposition.of(Registry.single(JedisPool.class, pool)));
         }
     };
+
+    @Before
+    public void init() {
+        when(pool.getResource()).thenReturn(jedis);
+    }
 
     @Test
     public void givenDefaultUrl_get404Response() {
@@ -86,15 +98,43 @@ public class ApplicationTest {
 
     @Test
     public void givenPostSpendUrl_Authorized_getOk() {
+        when(jedis.get(eq("transactions:C5EC7DCE6E38FA3A0E8BD4495AE49AFE")))
+                .thenReturn("[{\"date\":\"2019-10-19T13:57:40.109\",\"description\":\"initial setup\",\"amount\":\"100\",\"currency\":\"USD\"}]");
+        Transaction multi = mock(Transaction.class);
+        when(jedis.multi()).thenReturn(multi);
+        when(multi.exec()).thenReturn(Collections.singletonList(new Object()));
+
         ReceivedResponse post = appUnderTest.getHttpClient().requestSpec(r ->
-                r.getHeaders().set("Authorization", "Bearer test-auth-key")
+                r.body(b -> b.type("application/json")
+                        .text("{\"date\":\"2019-10-19T12:57:40.000\",\"description\":\"test transaction\",\"amount\":\"25\",\"currency\":\"USD\"}"))
+                .getHeaders().set("Authorization", "Bearer C5EC7DCE6E38FA3A0E8BD4495AE49AFE")
         ).post("/spend");
-        assertEquals(200, post.getStatusCode());
+        assertEquals(201, post.getStatusCode());
         assertEquals("ok", post.getBody().getText());
     }
 
     @Test
+    public void givenPostSpendUrl_Authorized_getOptimisticLockFail() {
+        when(jedis.get(eq("transactions:C5EC7DCE6E38FA3A0E8BD4495AE49AFE")))
+                .thenReturn("[{\"date\":\"2019-10-19T13:57:40.109\",\"description\":\"initial setup\",\"amount\":\"100\",\"currency\":\"USD\"}]");
+        Transaction multi = mock(Transaction.class);
+        when(jedis.multi()).thenReturn(multi);
+        when(multi.exec()).thenReturn(Collections.emptyList());
+
+        ReceivedResponse post = appUnderTest.getHttpClient().requestSpec(r ->
+                r.body(b -> b.type("application/json")
+                        .text("{\"date\":\"2019-10-19T12:57:40.000\",\"description\":\"test transaction\",\"amount\":\"25\",\"currency\":\"USD\"}"))
+                .getHeaders().set("Authorization", "Bearer C5EC7DCE6E38FA3A0E8BD4495AE49AFE")
+        ).post("/spend");
+        assertEquals(500, post.getStatusCode());
+        assertEquals("unable to complete request", post.getBody().getText());
+    }
+
+    @Test
     public void givenGetTransactionsUrl_Authorized_getOk() {
+        when(jedis.get(eq("transactions:C5EC7DCE6E38FA3A0E8BD4495AE49AFE")))
+                .thenReturn("[{\"date\":\"2019-10-19T13:57:40.109\",\"description\":\"initial setup\",\"amount\":\"100\",\"currency\":\"USD\"}]");
+
         ReceivedResponse get = appUnderTest.getHttpClient().requestSpec(r ->
                 r.getHeaders().set("Authorization", "Bearer C5EC7DCE6E38FA3A0E8BD4495AE49AFE")
         ).get("/transactions");
@@ -104,6 +144,7 @@ public class ApplicationTest {
 
     @Test
     public void givenGetBalanceUrl_Authorized_getOk() {
+        when(jedis.get(eq("balance:C5EC7DCE6E38FA3A0E8BD4495AE49AFE"))).thenReturn("USD 100");
         ReceivedResponse get = appUnderTest.getHttpClient().requestSpec(r ->
                 r.getHeaders().set("Authorization", "Bearer C5EC7DCE6E38FA3A0E8BD4495AE49AFE")
         ).get("/balance");
